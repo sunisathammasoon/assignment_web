047ee9ce28a6b4462c85e077b44a76dfaaaceccf
sunisathammasoon <5910110358@psu.ac.th>
2019-04-23 17:41:03 +0700

    component chat

--- src/Chat.js
+++ src/Chat.js
@@ -2,6 +2,15 @@
 import React from "react";
 
 class Chat extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            username: '',
+            message: '',
+            messages: []
+        };
+    }
     render(){
         return (
             <div className="container">
@@ -12,15 +21,15 @@ class Chat extends React.Component{
                                 <div className="card-title">Join Chat Here</div>
                                 <hr/>
                                 <div className="messages">
-                                    
+
                                 </div>
-                            </div>
-                            <div className="card-footer">
-                                    <input type="text" placeholder="Username" className="form-control"/>
+                                <div className="footer">
+                                    <input type="text" placeholder="Username" value={this.state.username} onChange={ev => this.setState({username: ev.target.value})} className="form-control"/>
                                     <br/>
-                                    <input type="text" placeholder="Message" className="form-control"/>
+                                    <input type="text" placeholder="Message" className="form-control" value={this.state.message} onChange={ev => this.setState({message: ev.target.value})}/>
                                     <br/>
                                     <button className="btn btn-primary form-control">Send</button>
+                                </div>
                             </div>
                         </div>
                     </div>
